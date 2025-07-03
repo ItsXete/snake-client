53e1658df6c9470281c6b24839ebdf718ea3819e
ItsXete <[dgiabach2000@gmail.com]>
2025-07-02 21:39:30 -0400

    working snake client

---
+++ play.js
@@ -0,0 +1,31 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server!");
+  });
+
+  conn.on("data", (data) => {
+    console.log("Server says:", data);
+  });
+
+  conn.on("error", (err) => {
+    console.error("Connection error:", err.message);
+  });
+
+  conn.on("end", () => {
+    console.log("Disconnected from server.");
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
